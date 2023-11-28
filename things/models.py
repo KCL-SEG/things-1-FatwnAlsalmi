2f854cb1f0c3f298e6905aee8a374be1d844f18d
Fatwn Alsalmi <fatwn.alsalmi@kcl.ac.uk>
2023-11-28 15:18:05 +0000

    Fix thing model

--- things/models.py
+++ things/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.core.validators import MaxLengthValidator
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
-    description = models.TextField(max_length=120, blank=True)
+    description = models.TextField(validators=[MaxLengthValidator(limit_value=120)], blank=True)
     quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
 
     def __str__(self):
